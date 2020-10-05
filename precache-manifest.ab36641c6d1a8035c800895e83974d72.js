799b239974cac1b20aa1c87d58d12ccbfe8b4c69
wildkiomi <whisann.lie14@gmail.com>
2020-10-05 18:42:37 +0300

    Updates

---
+++ precache-manifest.ab36641c6d1a8035c800895e83974d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc28e6aebe5b3987f75f517f2ce3a9e9",
+    "url": "/rick-morty/index.html"
+  },
+  {
+    "revision": "27eb8181140619e90bf2",
+    "url": "/rick-morty/static/css/main.9f950c2f.chunk.css"
+  },
+  {
+    "revision": "ef5c765fcba91c481374",
+    "url": "/rick-morty/static/js/2.cb178827.chunk.js"
+  },
+  {
+    "revision": "c7f92eccde8e496c47f87d08777731a5",
+    "url": "/rick-morty/static/js/2.cb178827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27eb8181140619e90bf2",
+    "url": "/rick-morty/static/js/main.bbb135c8.chunk.js"
+  },
+  {
+    "revision": "031736f702c37821150d",
+    "url": "/rick-morty/static/js/runtime-main.00f9065e.js"
+  }
+]);
